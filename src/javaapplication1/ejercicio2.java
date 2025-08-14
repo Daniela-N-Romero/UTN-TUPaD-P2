2456fcd5a127c8fd68e9395a8cd3367564d65973
Daniela Romero <danielanahirromero@gmail.com>
2025-08-14 07:59:27 -0300

    "Segundo commit - Ejercicio 2"

---
+++ src/javaapplication1/ejercicio2.java
@@ -0,0 +1,23 @@
+package javaapplication1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class ejercicio2 {
+    
+        public static void main(String[] args) {
+            String nombre = "Daniela";
+            int edad = 31;
+            double altura = 1.58;
+            boolean estudiante = true;
+            System.out.println("Hola soy "+nombre+" y tengo "+edad+" años. Mi altura es "+altura+".");
+            System.out.println("Estudiante: "+estudiante);
+    }
+    
+}
