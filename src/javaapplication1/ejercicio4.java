658454c52608bc4d6a69b33b5b1251e20507b18d
Daniela Romero <danielanahirromero@gmail.com>
2025-08-14 08:13:23 -0300

    "Tercer commit - Ejercicio 4"

--- src/javaapplication1/ejercicio4.java
+++ src/javaapplication1/ejercicio4.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author usuario
  */
-public class ejercicio3b {
+public class ejercicio4 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Ingresa tu nombre: ");
