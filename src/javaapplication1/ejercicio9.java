f3dfe67747a3487eeb060565d61e1c357ae0441f
Daniela Romero <danielanahirromero@gmail.com>
2025-08-15 17:09:14 -0300

    "Septimo commit - Ejercicio 9"

---
+++ src/javaapplication1/ejercicio9.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package javaapplication1;
+import java.util.Scanner; public class ejercicio9 {
+public static void main(String[] args) { 
+    Scanner scanner = new Scanner(System.in); 
+    System.out.print("Ingresa tu nombre: "); 
+    String nombre = scanner.nextLine(); 
+    System.out.println("Hola, " + nombre);
+}
+}
