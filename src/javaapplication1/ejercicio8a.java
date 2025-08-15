d7e9724db04f624d8ac1977dda1c1a9c04979851
Daniela Romero <danielanahirromero@gmail.com>
2025-08-15 16:57:21 -0300

    "Sexto commit - Ejercicio 8"

---
+++ src/javaapplication1/ejercicio8a.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package javaapplication1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class ejercicio8a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+        
+        System.out.println("Dividir de números enteros");
+        System.out.print("Ingresa el primer número (entero): ");
+        int num1 = Integer.parseInt(input.nextLine());
+        
+        System.out.print("Ingresa el segundo número (entero): ");
+        int num2 = Integer.parseInt(input.nextLine());
+        
+        int resultadoEntero = num1 / num2;
+        
+        System.out.println("El resultado de la división entera es: " + resultadoEntero);
+
+    }
+    
+}
