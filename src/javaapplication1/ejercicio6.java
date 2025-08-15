58d8898124926599be98ea0665955d3e3be099e5
Daniela Romero <danielanahirromero@gmail.com>
2025-08-15 16:30:07 -0300

    "Quinto commit - Ejercicio 6"

---
+++ src/javaapplication1/ejercicio6.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author usuario
+ */
+public class ejercicio6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        
+        System.out.println("Nombre: Juan Perez\n");
+        System.out.println("Edad: 30 años\n");
+        System.out.println("Dirección: \"Calle Falsa 123\"\n");
+        
+        
+    }
+}
